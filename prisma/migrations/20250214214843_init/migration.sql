f9cf4181aaaf1068431a0a414b5ff29456f154d8
Guizzs26 <guiteixeira1926@gmail.com>
2025-02-14 18:49:56 -0300

    feat: add user model

---
+++ prisma/migrations/20250214214843_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER', 'EMPLOYEE');
+
+-- CreateTable
+CREATE TABLE "customers" (
+    "id" TEXT NOT NULL,
+    "first_name" VARCHAR(127) NOT NULL,
+    "last_name" VARCHAR(127) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "avatar" VARCHAR(255),
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "customers_email_key" ON "customers"("email");
