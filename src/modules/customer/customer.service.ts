2c893948e0ef40db9a2dbee432c75910968cb5e7
Guizzs26 <guiteixeira1926@gmail.com>
2025-02-19 17:30:42 -0300

    refactor: add user creation flow with password hashing and email in use verification

--- src/modules/customer/customer.service.ts
+++ src/modules/customer/customer.service.ts
@@ -1,14 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/modules/prisma/prisma.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
+import { Customer } from 'generated/prisma_client';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class CustomerService {
   constructor(private prisma: PrismaService) {}
 
-  async create(createCustomerDto: CreateCustomerDto) {
-    return this.prisma.customer.create({
-      data: createCustomerDto,
+  async create(
+    createCustomerDto: CreateCustomerDto,
+  ): Promise<Omit<Customer, 'password'>> {
+    const existingCustomer = await this.prisma.customer.findUnique({
+      where: { email: createCustomerDto.email },
     });
+
+    if (existingCustomer) {
+      throw new ConflictException('Email already in use');
+    }
+
+    const hashedPassword = await hash(createCustomerDto.password, 10);
+
+    const customer = await this.prisma.customer.create({
+      data: {
+        ...createCustomerDto,
+        password: hashedPassword,
+      },
+    });
+
+    const { password, ...customerWithoutPassword } = customer;
+
+    return customerWithoutPassword;
   }
 }
